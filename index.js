422b0f91dca7393ae30e65d5b66786d7606ff6dc
Isabel Yasmin <bel.yasmin5@gmail.com>
2023-08-24 19:43:24 +0100

    salvando os arquivos

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const { Person } = require("./person");
+
+require("./modules/fs");
+
+ //require("./modules/path"); para de chamar e rodar o arquivo path na pasta modules
+//Se estiver rodando ,da ctl shif p e coloca clear terminal
+
+const person = new Person("Isabel");
+
---
+++ person.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayMyname () {
+        return `My name is ${this.name}!`;
+    }
+}
+
+module.exports = {
+    Person, 
+};
+
